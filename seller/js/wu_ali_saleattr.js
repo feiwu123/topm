247ac3c2c6922e41f567f75b485d655f7857437d
flywu <739842930@qq.com>
2025-04-30 17:31:01 +0800

    '新'

--- seller/js/wu_ali_saleattr.js
+++ seller/js/wu_ali_saleattr.js
@@ -8,7 +8,7 @@ jQuery.multialiSaleProps = function(divselectid, data) {
 		<div class="sale_attr_elem" >
 			<div class="sale_attr_title">
 				<div class="title">
-					<span class="saleAttrName"></span><span class="red">*</span>
+					<span class="saleAttrName"></span><span class="requiredIcon red">*</span>
 				</div>
 			</div>
 			<div class="sale_attr_name attrVal">
@@ -30,41 +30,44 @@ jQuery.multialiSaleProps = function(divselectid, data) {
 		</div>
 	</div>`;
 		var template2 = `<div class="elems sale_attr_value">
-									<div class="title">
-										<span class="saleAttrName">Color</span><span class="red">*</span>
-									</div>
-									<div class="sale_attr_val">
-										<div class="sale_attr_val_item">
-											<div class="option_val attrgroups">
-												<div class="sale_attr_select">
-													<div class="attrVal">
-														<div class="attr_select_val">
-														</div>
-														<div class="icons"><span class="iconfont icon-ICON-xia"></span></div>
-														<div class="attrVal_dropList" style="display: none; top: 40px;">
-															<ul>
-																
-															</ul>	
-														</div>
+							<div class="sale_attr_elem">
+								<div class="title">
+									<span class="saleAttrName">Color</span><span class="requiredIcon red">*</span>
+								</div>
+								<div class="sale_attr_val">
+									<div class="sale_attr_val_item">
+										<div class="option_val attrgroups">
+											<div class="sale_attr_select">
+												<div class="attrVal">
+													<div class="attr_select_val">
+													</div>
+													<div class="icons"><span class="iconfont icon-ICON-xia"></span></div>
+													<div class="attrVal_dropList sale_attr_select_list" style="display: none; top: 40px;">
+														<ul>
+															
+														</ul>	
 													</div>
 												</div>
-												<div class="delete_sale_attr">
-													删除
-												</div>
+											</div>
+											<div class="delete_sale_attr">
+												删除
 											</div>
 										</div>
 									</div>
-									<div class="confirmSaleAttr">
-										<span class="iconfont icon-xinzeng"></span><span>添加更多</span>
-									</div>
-								</div>`;
+								</div>
+								<div class="confirmSaleAttr addoneAttr">
+									<span class="iconfont icon-xinzeng"></span><span>添加更多</span>
+								</div>
+								<div class="red error_tips">
+								</div>
+							</div>
+						</div>`;
     if (!Array.isArray(fields)) {
 		fields = new Array(fields); 	
 	}
 	if (Array.isArray(fields)) {
 		fields.forEach((fieldItem) => {
 			var rules = checkSalePropsRule(fieldItem.rules.rule);
-			console.log(rules);
 			var options = fieldItem.options.option;
 			var fieldName = fieldItem['@attributes']['name'];
 			var fieldId = fieldItem['@attributes']['id'];
@@ -72,8 +75,8 @@ jQuery.multialiSaleProps = function(divselectid, data) {
 			var listTemplate = "";
 			var uploadImgTml = "";
 			var showTpl = "";
-			var createTpl = $(template1);
-			var createTpl2 = $(template2);
+			var createTpl = $(template1).clone();
+			var createTpl2 = $(template2).clone();
 		
 			//只有一个输入框
 			if(options.length==1 && options[0]['@attributes']['displayName']=="other" && options[0]['@attributes']['value']=="-1"){
@@ -88,14 +91,17 @@ jQuery.multialiSaleProps = function(divselectid, data) {
 					var optionVal = item['@attributes']['value'];
 					var optionName = item['@attributes']['displayName'];
 					if(rules.canRemark){
-						listTemplate += `<li value="${optionVal}" tabindex="-1" role="menuitem" class="next-menu-item">
-											<div class="item-block">
-												<div class="block-inner" role="div-color" style="height: 10px; width: 10px; background: ${optionName};"></div>
-											</div>
+						// listTemplate += `<li value="${optionVal}" tabindex="-1" role="menuitem" class="next-menu-item" data-name="${optionName}">
+						// 					<div class="item-block">
+						// 						<div class="block-inner" role="div-color" style="height: 10px; width: 10px; background: ${optionName};"></div>
+						// 					</div>
+						// 					<div class="item-value" role="color-text">${optionName}</div>
+						// 			</li>`;
+						listTemplate += `<li value="${optionVal}" tabindex="-1" role="menuitem" class="next-menu-item" data-name="${optionName}">
 											<div class="item-value" role="color-text">${optionName}</div>
-									</li>`;
+									</li>`;			
 					}else{
-						listTemplate += `<li value="${optionVal}" tabindex="-1" role="menuitem" class="next-menu-item sale_attr_select_item">
+						listTemplate += `<li value="${optionVal}" tabindex="-1" role="menuitem" class="next-menu-item sale_attr_select_item" data-name="${optionName}">
 												<input type="checkbox" name="attr_${optionVal}"  id="checkbox_${fieldId}_${optionVal}" data-attrid="${optionVal}" value="${optionName}" />
 												<label for="checkbox_${fieldId}_${optionVal}">${optionName}</label>
 										</li>`;
@@ -110,15 +116,57 @@ jQuery.multialiSaleProps = function(divselectid, data) {
 											
 										</div>
 									</div>`;
-									
+					if(!rules.is_requried){
+						createTpl2.find(".requiredIcon").remove();
+					}
 					createTpl2.find(".sale_attr_val_item").prepend(uploadImgTml);		
 					createTpl2.find(".sale_attr_select ul").append(listTemplate);
 					createTpl2.find(".sale_attr_elem").attr("id","sale_attr_"+fieldId);
 					createTpl2.find(".saleAttrName").html(fieldName);
 					createTpl2.find(".sale_attr_select ul").perfectScrollbar("destroy");
 					createTpl2.find(".sale_attr_select ul").perfectScrollbar();
+					var idName = "#sale_attr_"+fieldId;
+					$(document).on("click",idName+' .attrVal ul li',function(event){
+						event.stopImmediatePropagation();
+						var name = $(this).data("name");
+						var value = $(this).attr("value");
+						$(this).parents(".attrVal").find(".attr_select_val").html("<span data-name='"+name+"' data-value='"+value+"'>"+name+"</span>");
+						$(idName).find(".sale_attr_select_list").hide();
+						if($(idName).data("select_value")){
+							var select_vals = $(idName).data("select_value")+','+value;
+							$(idName).data("select_value",select_vals);
+						}else{
+							$(idName).data("select_value",value);
+						}
+						
+					})
+					$(document).on("click",idName+' .addoneAttr',function(event){
+						var rowNum = $(idName).find(".sale_attr_val .sale_attr_val_item").length;
+						if(rowNum>=Number(rules.maxValueRule)){
+							$(idName).find(".error_tips").append("<div>*最多添加"+rules.maxValueRule+"条</div>");
+							return;
+						}
+						var template = createTpl2.find(".sale_attr_val_item").eq(0).clone();
+						$(template).find(".attr_select_val").html('');
+						var select_vals = $(idName).data('select_value')?$(idName).data('select_value').split(","):[];
+						$(template).find("ul li").each((index,item)=>{
+							var liId = $(item).attr("value");
+							if(select_vals.includes(liId)){
+								$(item).hide();
+							}
+						})
+						$(idName).find(".sale_attr_val").append(template);
+						$(idName).find(".sale_attr_select ul").perfectScrollbar("destroy");
+						$(idName).find(".sale_attr_select ul").perfectScrollbar();
+					})
+					$(document).on("click",idName+' .delete_sale_attr',function(event){
+						$(this).parents(".sale_attr_val_item").remove();
+					})
 					renderHtml.append(createTpl2);
 				}else{
+					if(!rules.is_requried){
+						createTpl.find(".requiredIcon").remove();
+					}
 					createTpl.find(".sale_attr_val_item").prepend(uploadImgTml);
 					createTpl.find(".sale_attr_select ul").append(listTemplate);
 					createTpl.find(".sale_attr_elem").attr("id","sale_attr_"+fieldId);
@@ -140,7 +188,7 @@ jQuery.multialiSaleProps = function(divselectid, data) {
 					$(document).on("click",idName+' .attrVal .attr_select_val .icon-guanbi',function(event){
 						event.stopImmediatePropagation();
 						var attr_val_id = $(this).data('attrid');
-						var chebox_id = "#checkbox_"+attr_val_id;
+						var chebox_id = "#checkbox_"+fieldId+"_"+attr_val_id;
 						$(this).parent('.selected_val').remove();
 						$(chebox_id).prop('checked',false);
 					})
